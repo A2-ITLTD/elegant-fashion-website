ad07ec955f21a558f78704743fe92fde76b34688
NahidaFatme <enin.mimix13@gmail.com>
2024-07-01 18:54:43 +0600

    why choose us section done

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -5,6 +5,10 @@ import { BiSolidRightArrow } from "react-icons/bi";
 import bg1 from "../Images/bg1.jpg";
 import bg2 from '../Images/bg2.jpg'; 
 import bg3 from '../Images/bg3.jpg';
+import check from '../Images/checked.png';
+import supply from '../Images/supply.png';
+import hand from '../Images/hand.png';
+import staff from '../Images/staff.png';
 const Landingpage = () => {
     
     const [currentSlide, setCurrentSlide] = useState(1);
@@ -48,7 +52,35 @@ const Landingpage = () => {
             </section>
             {/* why choose us */}
             <section className='mt-24'>
-                <div></div>
+                <div className='text-center text-5xl font-semibold pb-20'>
+                    <h1>Why Choose Us</h1>
+                </div>
+                <div className='flex items-center justify-evenly gap-8 px-24 text-center '>
+                    <div className='border-2 border-slate-900 p-8 flex flex-col items-center gap-6'>
+                        <img src={check} alt="checked icon" className='w-36'/>
+                        <h1 className='text-2xl font-semibold'>site-based material examination</h1>
+                        <p className='text-sm font-normal'> On-site inspections to verify fabric quality, ensure adherence to specifications, and maintain consistent product standards.
+                        </p>
+                    </div>
+                    <div className='border-2 border-slate-900 p-8 flex flex-col items-center  gap-6'>
+                        <img src={supply} alt="checked icon" className='w-36'/>
+                        <h1 className='text-2xl font-semibold'>procedures for supplier evaluation  </h1>
+                        <p className='text-sm font-normal'>Comprehensive assessment of suppliers based on quality, reliability, compliance, and performance to ensure optimal partnerships.
+                        </p>
+                    </div>
+                    <div className='border-2 border-slate-900 p-8 flex flex-col items-center  gap-6'>
+                        <img src={hand} alt="checked icon" className='w-36'/>
+                        <h1 className='text-2xl font-semibold'>complete customization </h1>
+                        <p className='text-sm font-normal'>Offering fully tailored designs and manufacturing solutions to meet unique client requirements, styles, and preferences.
+                        </p>
+                    </div>
+                    <div className='border-2 border-slate-900 p-8 flex flex-col items-center  gap-6'>
+                        <img src={staff} alt="checked icon" className='w-36'/>
+                        <h1 className='text-2xl font-semibold'>overall trading personnel</h1>
+                        <p className='text-sm font-normal'>Experienced team managing procurement, quality control, and client relations to ensure smooth and efficient operations.
+                        </p>
+                    </div>
+                </div>
             </section>
         </div>
     );
