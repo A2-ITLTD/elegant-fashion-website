42735922271468e167e9fef4cf72d4b5beefa041
NahidaFatme <enin.mimix13@gmail.com>
2024-07-28 15:42:05 +0600

    bug fixed

--- src/Components/ProfileAbout.jsx
+++ src/Components/ProfileAbout.jsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom';
 import i4 from '../Images/about4.jpg'; 
 import i5 from '../Images/about5.png'; 
 import i6 from '../Images/about6.png'; 
+import i11 from '../Images/about11.jpg'; 
 const ProfileAbout = () => {
     return (
         <div className='mt-12 w-full px-5 md:px-0 md:w-3/6 mx-auto'>
             <div className='flex flex-col gap-10'>
-             <h1 className="text-center text-5xl font-semibold">Who Are We</h1>
+             <h1 className="text-center text-5xl font-semibold">Who We Are</h1>
              <p className="text-base text-justify">At Elegant Fashion, we believe in a world where everyone has the freedom to be themselves, without judgement. We encourage experimentation and self-expression, empowering you to embrace life as the extraordinary adventure it is. Our mission is to ensure that everyone has an equal opportunity to explore and showcase their unique style, regardless of their background or preferences. We are here to give you the confidence to be whoever you want to be.</p>
              <img src={i4} className='h-[420px] w-full object-cover object-center' />
              <h1 className="text-left text-3xl font-semibold">Fashion democracy - Choice for all</h1>
@@ -17,6 +18,7 @@ const ProfileAbout = () => {
              <img src={i6} className='h-[420px] w-full object-cover object-center' />
              <h1 className="text-left text-3xl font-semibold">Elegant Marketplace</h1>
              <p className="text-base text-justify">There's nothing quite like the thrill of discovering an incredible vintage boutique or an amazing independent brand before everyone else. We love that feeling too, which is why we launched Elegant Fashion Marketplace in 2024. Our team curates the best fashion start-ups, giving you the chance to shop unique finds all the time.</p>
+             <img src={i11} className='h-[420px] w-full object-cover object-center' />
              <div className='flex flex-col gap-3 text-center mt-5'>
                 <h1 className="text-3xl font-semibold">Our Legal Info</h1>
                 <p className="text-base font-semibold">Company Name: Elegant Fashion</p>
